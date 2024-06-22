b87e60980be6f3652a009586198c8869e6da5ee8
pratapyadavmca <72945849+pratapyadavmca@users.noreply.github.com>
2024-06-22 23:33:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangerSignConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                 uuuuuuu");
+            Console.WriteLine("             uu$$$$$$$$$$$uu");
+            Console.WriteLine("          uu$$$$$$$$$$$$$$$$$uu");
+            Console.WriteLine("         u$$$$$$$$$$$$$$$$$$$$$u");
+            Console.WriteLine("        u$$$$$$$$$$$$$$$$$$$$$$$u");
+            Console.WriteLine("       u$$$$$$$$$$$$$$$$$$$$$$$$$u");
+            Console.WriteLine("       u$$$$$$$$$$$$$$$$$$$$$$$$$u");
+            Console.WriteLine("       u$$$$$$~   ~$$$~   ~$$$$$$u");
+            Console.WriteLine("       ~$$$$~      u$u       $$$$~");
+            Console.WriteLine("        $$$u       u$u       u$$$");
+            Console.WriteLine("        $$$u      u$$$u      u$$$");
+            Console.WriteLine("         ~$$$$uu$$$   $$$uu$$$$~");
+            Console.WriteLine("          ~$$$$$$$~   ~$$$$$$$~");
+            Console.WriteLine("            u$$$$$$$u$$$$$$$u");
+            Console.WriteLine("             u$~$~$~$~$~$~$u");
+            Console.WriteLine("  uuu        $$u$ $ $ $ $u$$       uuu");
+            Console.WriteLine(" u$$$$        $$$$$u$u$u$$$       u$$$$");
+            Console.WriteLine("  $$$$$uu      ~$$$$$$$$$~     uu$$$$$$");
+            Console.WriteLine("u$$$$$$$$$$$uu    ~~~~~    uuuu$$$$$$$$$$");
+            Console.WriteLine("$$$$~~~$$$$$$$$$$uuu   uu$$$$$$$$$~~~$$$~");
+            Console.WriteLine(" ~~~      ~~$$$$$$$$$$$uu ~~$~~~");
+            Console.WriteLine("           uuuu ~~$$$$$$$$$$uuu");
+            Console.WriteLine("  u$$$uuu$$$$$$$$$uu ~~$$$$$$$$$$$uuu$$$");
+            Console.WriteLine("  $$$$$$$$$$~~~~           ~~$$$$$$$$$$$~");
+            Console.WriteLine("   ~$$$$$~                      ~~$$$$~~");
+            Console.WriteLine("     $$$~                         $$$$~");
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("**                                          **");
+            Console.WriteLine("** !!!!! Ha ha ha Enjoy Coding !!!!! **");
+            Console.ReadLine();
+        }
+    }
+}
